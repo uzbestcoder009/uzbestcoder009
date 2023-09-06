4a2ff9af7ff8a48583ff78a29576cc403e98f761
uzbestcoder009 <144257721+uzbestcoder009@users.noreply.github.com>
2023-09-06 21:05:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+//while--------------5
+
+
+
+/*
+let n = + prompt("Son kiriting");
+let k = 2;
+let s = 0;
+while(n>=k){
+
+	n /= k
+	s++
+}
+
+ console.log(n===1 ? 'Kiritilgan son 2-ning '+ s + '-darajasi' : 'Kiritilgan son 2-ning darajasi emas')*/
+
+
+
+
+
+//while--------------8
+
+
+
+/*let n = 98;
+let k = 8;
+
+
+
+while(n>k*k){
+	 n--
+} 
+   console.log(n)
+	 */
+
+//while--------------9
+
+/*
+let k = 5;
+let n = 24;
+
+while(3**k>n){
+	k--
+}
+console.log(k)
+*/
+
+//while--------------10c
+/*
+let k = 2;
+let n = 100;
+
+while(3**k<n){
+	k++
+}
+console.log(k)
+*/
+
+//while--------------11
+/*
+let n = 25;
+let k = 1;
+let s = 0;
+
+while(n>=k){
+	k++
+	s += k
+console.log(k)
+console.log(s)
+
+}*/
